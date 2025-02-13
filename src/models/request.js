67da50cad408ed8b277dedd121274be11e0b90eb
ao627515 <ao627515@gmail.com>
2025-02-13 02:57:51 +0000

    `Added User import and properties to Request model`

--- src/models/request.js
+++ src/models/request.js
@@ -1,4 +1,10 @@
-
+import { User } from './user';
 export class Request {
-
+  _id = undefined;
+  _acceptButtonText = 'Oui';
+  _text = 'Demande ?';
+  forceAccept = false;
+  isAccepted = false;
+  isRejected = false;
+  _user = new User();
 }
