8bda440a9495db470d3f91887f88fb760497d460
ao627515 <ao627515@gmail.com>
2025-02-13 02:54:25 +0000

    `Added id property and getter/setter to AbstractUser class`

--- src/models/abstract-user.js
+++ src/models/abstract-user.js
@@ -1,11 +1,20 @@
 
 export class AbstractUser {
+  id = undefined;
   _email = null;
 
   constructor() {
     if (new.target === AbstractUser) {
       throw new Error("AbstractUser est une classe abstraite et ne peut pas être instanciée directement.");
     }
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  set id(id) {
+    this._id = id;
   }
 
   get email() {
